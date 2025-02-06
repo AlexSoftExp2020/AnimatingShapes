83a932cd2b64dc2e76acd725ffe0c4b6bad0c51f
Alex <oleksii.pidhornyi@gmail.com>
2025-02-06 21:04:24 +0100

    Initial Commit

---
+++ AnimatingShapes/AnimatingShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatingShapesApp.swift
+//  AnimatingShapes
+//
+//  Created by Oleksii on 06.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimatingShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
