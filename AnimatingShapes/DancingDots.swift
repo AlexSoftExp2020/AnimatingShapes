88fdb3239310770a7664da01ff90e0f969d3fc10
Alex <oleksii.pidhornyi@gmail.com>
2025-02-06 22:42:27 +0100

    add dancing dots

---
+++ AnimatingShapes/DancingDots.swift
@@ -0,0 +1,18 @@
+//
+//  DancingDots.swift
+//  AnimatingShapes
+//
+//  Created by Oleksii on 06.02.2025.
+//
+
+import SwiftUI
+
+struct DancingDots: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DancingDots()
+}
