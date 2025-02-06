3da54d1df0910fa0236179dce39ecdacfbc57c9f
Alex <oleksii.pidhornyi@gmail.com>
2025-02-06 22:57:46 +0100

    add big dot

--- AnimatingShapes/DancingDots.swift
+++ AnimatingShapes/DancingDots.swift
@@ -14,6 +14,37 @@ class SmallDot: Identifiable, ObservableObject {
     @Published var color: Color = .primary
 }
 
+class BigDot: Identifiable, ObservableObject {
+    let id = UUID()
+    
+    @Published var offset: CGSize = .zero
+    @Published var color: Color = .primary
+    @Published var scale: Double = 1.0
+    @Published var smallDots = [SmallDot]()
+    
+    init() {
+        for _ in 0..<5 {
+            smallDots.append(SmallDot())
+        }
+    }
+    
+    func randomizePositions() {
+        objectWillChange.send()
+        for dot in smallDots {
+            dot.offset = CGSize(width: Double.random(in: -120...120), height: Double.random(in: -120...120))
+            dot.color = DotTracker.randomColor
+        }
+    }
+    
+    func resetPositions() {
+        objectWillChange.send()
+        for dot in smallDots {
+            dot.offset = .zero
+            dot.color = .primary
+        }
+    }
+}
+
 struct DancingDots: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
