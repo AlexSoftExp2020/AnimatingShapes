dce43b2bdfbb38b1791fb27469426ef528c65ba6
Alex <oleksii.pidhornyi@gmail.com>
2025-02-06 21:13:45 +0100

    add basic animation view

---
+++ AnimatingShapes/BasicAnimationView.swift
@@ -0,0 +1,21 @@
+//
+//  BasicAnimationView.swift
+//  AnimatingShapes
+//
+//  Created by Oleksii on 06.02.2025.
+//
+
+import SwiftUI
+
+struct BasicAnimationView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    BasicAnimationView()
+}
