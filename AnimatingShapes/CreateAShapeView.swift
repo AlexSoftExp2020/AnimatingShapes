7587108241e9991edea8a2f8c9789f4747a7b7b9
Alex <oleksii.pidhornyi@gmail.com>
2025-02-06 22:40:04 +0100

    add create a shape view

---
+++ AnimatingShapes/CreateAShapeView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateAShapeView.swift
+//  AnimatingShapes
+//
+//  Created by Oleksii on 06.02.2025.
+//
+
+import SwiftUI
+
+struct CreateAShapeView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    CreateAShapeView()
+}
