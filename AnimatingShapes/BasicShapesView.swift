bd5b68ab4d1e34a4f70ee8296de10d8bb1922c38
Alex <oleksii.pidhornyi@gmail.com>
2025-02-06 21:19:26 +0100

    add basic shapes view

---
+++ AnimatingShapes/BasicShapesView.swift
@@ -0,0 +1,23 @@
+//
+//  BasicShapesView.swift
+//  AnimatingShapes
+//
+//  Created by Oleksii on 06.02.2025.
+//
+
+import SwiftUI
+
+struct BasicShapesView: View {
+//    @State private var trimStart: Double = 0
+//    @State private var trimEnd: Double = 1
+//    @State private var width: Double = 1
+//    @State private var height: Double = 1
+//    @State private var selectedShape: ShapeType = .circle
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BasicShapesView()
+}
