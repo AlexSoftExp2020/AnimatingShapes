d787af7081cd8196ba0e79d8b4743f246501279f
Alex <oleksii.pidhornyi@gmail.com>
2025-02-06 22:02:46 +0100

    add composing shapes view

---
+++ AnimatingShapes/ComposingShapesView.swift
@@ -0,0 +1,18 @@
+//
+//  ComposingShapesView.swift
+//  AnimatingShapes
+//
+//  Created by Oleksii on 06.02.2025.
+//
+
+import SwiftUI
+
+struct ComposingShapesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ComposingShapesView()
+}
