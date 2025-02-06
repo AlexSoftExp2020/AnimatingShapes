a6968839b62c682aa06bf0afafcc2bc4872ba750
Alex <oleksii.pidhornyi@gmail.com>
2025-02-06 22:45:58 +0100

    add small dot

--- AnimatingShapes/DancingDots.swift
+++ AnimatingShapes/DancingDots.swift
@@ -7,6 +7,13 @@
 
 import SwiftUI
 
+class SmallDot: Identifiable, ObservableObject {
+    let id = UUID()
+    
+    @Published var offset: CGSize = .zero
+    @Published var color: Color = .primary
+}
+
 struct DancingDots: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
