8da4298cf1b617de9b4c7305a0bf68c79422429e
Alex <oleksii.pidhornyi@gmail.com>
2025-02-07 22:52:56 +0100

    add drag n drop dots

---
+++ AnimatingShapes/DragNDropDots.swift
@@ -0,0 +1,18 @@
+//
+//  DragNDropDots.swift
+//  AnimatingShapes
+//
+//  Created by Oleksii on 07.02.2025.
+//
+
+import SwiftUI
+
+struct DragNDropDots: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DragNDropDots()
+}
