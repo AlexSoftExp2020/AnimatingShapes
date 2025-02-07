fb8f062445106481d501609e13c039ac4c11c71a
Alex <oleksii.pidhornyi@gmail.com>
2025-02-07 22:18:22 +0100

    add shape button style

---
+++ AnimatingShapes/ShapeButtonStyle.swift
@@ -0,0 +1,18 @@
+//
+//  ShapeButtonStyle.swift
+//  AnimatingShapes
+//
+//  Created by Oleksii on 07.02.2025.
+//
+
+import SwiftUI
+
+struct ShapeButtonStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShapeButtonStyle()
+}
