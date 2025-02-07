e18bad1abb6c76bd13c019a2dbe2fb3e53fb0b35
Alex <oleksii.pidhornyi@gmail.com>
2025-02-07 22:24:29 +0100

    add shape button

--- AnimatingShapes/ShapeButtonStyle.swift
+++ AnimatingShapes/ShapeButtonStyle.swift
@@ -7,6 +7,19 @@
 
 import SwiftUI
 
+struct ShapeButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration
+            .label
+            .padding()
+            .frame(minWidth: 125, minHeight: 60)
+            .background(Color.blue.opacity(0.15))
+            .foregroundColor(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 15))
+            .padding(.bottom, 30)
+    }
+}
+
 struct ShapeButtonStyle: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
