786daaf54f7eebf1eb9b764e8b5515a563a6701e
Alex <oleksii.pidhornyi@gmail.com>
2025-02-08 15:31:27 +0100

    add reset heart

--- AnimatingShapes/HeartPulse.swift
+++ AnimatingShapes/HeartPulse.swift
@@ -36,6 +36,16 @@ struct Heart: Shape {
     }
 }
 
+struct ResetHeart: View {
+    var body: some View {
+        Heart()
+            .frame(width: 100, height: 100)
+            .foregroundColor(.red)
+            .shadow(color: .pink, radius: 10)
+            .frame(width: 300, height: 300)
+    }
+}
+
 struct HeartPulse: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
