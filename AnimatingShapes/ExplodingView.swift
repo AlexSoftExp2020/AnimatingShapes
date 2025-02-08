f958b68a2038e575e51b3fedcbfa622ec6fa025f
Alex <oleksii.pidhornyi@gmail.com>
2025-02-08 13:11:04 +0100

    extend exploding view

--- AnimatingShapes/ExplodingView.swift
+++ AnimatingShapes/ExplodingView.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct ExplodingView: View {
+    @State private var isExploded = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            ZStack {
+                ForEach(0..<75) { _ in
+                    Rectangle()
+                        .rotation(Angle(degrees: Double.random(in: 0..<360)))
+                        .frame(width: 20, height: 20)
+                        .foregroundColor(.indigo)
+                        .offset(x: isExploded ? (Double.random(in: -1...1) * 500) : 0, y: isExploded ? (Double.random(in: -1...1) * 500) : 0)
+                        .opacity(isExploded ? 0 : 1)
+                        .animation(.easeInOut.speed(0.6), value: isExploded)
+                        .padding()
+                }
+                Rectangle()
+                    .frame(width: 300, height: 300)
+                    .foregroundColor(.indigo)
+                    .opacity(isExploded ? 0 : 1)
+                    .animation(.easeInOut.speed(0.6), value: isExploded)
+                    .padding()
+            }
+            Spacer()
+            PlayResetButton(animating: $isExploded)
+        }
+        .navigationTitle("Explosion Animation")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
