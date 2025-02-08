0994980989eda1396d62407436804f463b018b27
Alex <oleksii.pidhornyi@gmail.com>
2025-02-08 17:42:37 +0100

    add modify shapes

---
+++ AnimatingShapes/ModifyShapes.swift
@@ -0,0 +1,20 @@
+//
+//  ModifyShapes.swift
+//  AnimatingShapes
+//
+//  Created by Oleksii on 08.02.2025.
+//
+
+import SwiftUI
+
+struct ModifyShapes: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ModifyShapes()
+}
