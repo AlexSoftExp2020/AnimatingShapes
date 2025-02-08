1940ba13b8e0d3f0bac80515c46ded008adf9d4e
Alex <oleksii.pidhornyi@gmail.com>
2025-02-08 17:43:55 +0100

    add rollin rainbow

---
+++ AnimatingShapes/RollinRainbow.swift
@@ -0,0 +1,18 @@
+//
+//  RollinRainbow.swift
+//  AnimatingShapes
+//
+//  Created by Oleksii on 08.02.2025.
+//
+
+import SwiftUI
+
+struct RollinRainbow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RollinRainbow()
+}
