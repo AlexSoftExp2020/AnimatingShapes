a37eda01ae2bfc2bb8bb03b72720aef50e79be7d
Alex <oleksii.pidhornyi@gmail.com>
2025-02-08 12:53:20 +0100

    add exploding view

---
+++ AnimatingShapes/ExplodingView.swift
@@ -0,0 +1,18 @@
+//
+//  ExplodingView.swift
+//  AnimatingShapes
+//
+//  Created by Oleksii on 08.02.2025.
+//
+
+import SwiftUI
+
+struct ExplodingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExplodingView()
+}
