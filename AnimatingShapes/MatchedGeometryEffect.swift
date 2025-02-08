3305c4d567bd304030502d020f386a30355d1e96
Alex <oleksii.pidhornyi@gmail.com>
2025-02-08 17:31:27 +0100

    add circle to matched geometry effect view

--- AnimatingShapes/MatchedGeometryEffect.swift
+++ AnimatingShapes/MatchedGeometryEffect.swift
@@ -7,12 +7,21 @@
 
 import SwiftUI
 
-struct MatchedGeometryEffect: View {
+struct MatchedGeometryEffectView: View {
+    @State var exchange = false
+    
+    private func circle() -> some View {
+        Circle()
+            .fill(.red)
+            .frame(width: 44, height: 44)
+    }
+    
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    MatchedGeometryEffect()
+    MatchedGeometryEffectView()
 }
