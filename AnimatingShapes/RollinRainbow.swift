8f4e36ce06bb3a13dd3534389c3cce1cd28da918
Alex <oleksii.pidhornyi@gmail.com>
2025-02-09 23:56:00 +0100

    add rollin bow

--- AnimatingShapes/RollinRainbow.swift
+++ AnimatingShapes/RollinRainbow.swift
@@ -7,12 +7,36 @@
 
 import SwiftUI
 
-struct RollinRainbow: View {
+struct RollinRainbowView: View {
+    @State private var isAnimating = false
+    @State private var gridColumns = Array(repeating: GridItem(.flexible()), count: 10)
+    @State private var colors: [Color] = [.pink, .mint, .orange, .teal, .yellow, .cyan, .purple, .blue]
+    @State private var scaleFactor: CGFloat = 3
+    
+    let springAnimation = Animation.spring(response: 0.4, dampingFraction: 0.75, blendDuration: 0.9)
+    
+    let numCircles = 100
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Tap on the dots to make them roll!")
+                .padding(.vertical, 30)
+            
+            Spacer()
+            LazyVGrid(columns: gridColumns) {
+                ForEach(0..<numCircles, id: \.self) { index in
+                    Circle()
+                        .foregroundColor(colors[index % colors.count])
+                        .scaleEffect(scaleFactor)
+                    // MARK: TODO
+                }
+            }
+        }
+        .navigationTitle("Rollin' Rainbow")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    RollinRainbow()
+    RollinRainbowView()
 }
