9079d4ca200f5d84bf75f52f36965f10eb874a82
Alex <oleksii.pidhornyi@gmail.com>
2025-02-10 20:10:33 +0100

    add destination

--- AnimatingShapes/TopicOrganizer.swift
+++ AnimatingShapes/TopicOrganizer.swift
@@ -7,6 +7,17 @@
 
 import SwiftUI
 
+enum Destination: String, CaseIterable {
+    case basicShapes
+    case simpleAnimation
+    case spinningAnimation
+    case explodingAnimation
+    case rollingRainbow
+    case dragnDropDots
+    case dancingDots
+}
+
+
 struct TopicOrganizer: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
