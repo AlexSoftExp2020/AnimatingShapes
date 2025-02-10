6c5bda4372d89c61acf206bb04271b783748740b
Alex <oleksii.pidhornyi@gmail.com>
2025-02-10 20:20:27 +0100

    remove topic organizer

--- AnimatingShapes/TopicOrganizer.swift
+++ AnimatingShapes/TopicOrganizer.swift
@@ -84,14 +84,3 @@ struct TopicData {
         )
     ]
 }
-
-
-struct TopicOrganizer: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    TopicOrganizer()
-}
