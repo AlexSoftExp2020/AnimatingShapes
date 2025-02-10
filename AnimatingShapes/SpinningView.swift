5e46badc1e7caa08edf23937e0bfb95cf08ab67b
Alex <oleksii.pidhornyi@gmail.com>
2025-02-10 19:11:14 +0100

    add reset ellipse

--- AnimatingShapes/SpinningView.swift
+++ AnimatingShapes/SpinningView.swift
@@ -7,6 +7,14 @@
 
 import SwiftUI
 
+struct ResetEllipse: View {
+    var body: some View {
+        Ellipse()
+            .frame(width: 200, height: 200)
+            .foregroundColor(.orange)
+    }
+}
+
 struct SpinningView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
