8f30ca2ba135604d1ce22cc46902befdbfea98f6
Alex <oleksii.pidhornyi@gmail.com>
2025-02-10 20:07:22 +0100

    add topic organizer

---
+++ AnimatingShapes/TopicOrganizer.swift
@@ -0,0 +1,18 @@
+//
+//  TopicOrganizer.swift
+//  AnimatingShapes
+//
+//  Created by Oleksii on 10.02.2025.
+//
+
+import SwiftUI
+
+struct TopicOrganizer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TopicOrganizer()
+}
