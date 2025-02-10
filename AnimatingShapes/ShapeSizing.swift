56a849fed91de9746f190ff688e38166df3c0a61
Alex <oleksii.pidhornyi@gmail.com>
2025-02-10 19:02:37 +0100

    add shape sizing

---
+++ AnimatingShapes/ShapeSizing.swift
@@ -0,0 +1,18 @@
+//
+//  ShapeSizing.swift
+//  AnimatingShapes
+//
+//  Created by Oleksii on 10.02.2025.
+//
+
+import SwiftUI
+
+struct ShapeSizing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShapeSizing()
+}
