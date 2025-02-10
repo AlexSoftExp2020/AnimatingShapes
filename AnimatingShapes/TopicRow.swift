56b5a87cf466d884a5d21e2f8cdf022f22c4bb84
Alex <oleksii.pidhornyi@gmail.com>
2025-02-10 20:21:33 +0100

    add topic row

---
+++ AnimatingShapes/TopicRow.swift
@@ -0,0 +1,18 @@
+//
+//  TopicRow.swift
+//  AnimatingShapes
+//
+//  Created by Oleksii on 10.02.2025.
+//
+
+import SwiftUI
+
+struct TopicRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TopicRow()
+}
