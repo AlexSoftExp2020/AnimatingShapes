c21c83ab9f38ea602148c628b2a27996e88415ec
Alex <oleksii.pidhornyi@gmail.com>
2025-02-10 20:19:41 +0100

    add topic data

--- AnimatingShapes/TopicOrganizer.swift
+++ AnimatingShapes/TopicOrganizer.swift
@@ -37,6 +37,54 @@ enum Destination: String, CaseIterable {
     }
 }
 
+struct TopicData {
+    static var homeContent = [
+        Topic(
+            title: "Basic Shapes",
+            description: "Experiment with different shape types.",
+            systemSymbol: "square.on.circle",
+            destination: .basicShapes
+        ),
+        Topic(
+            title: "Basic Animation",
+            description: "See an example of a basic animation.",
+            systemSymbol: "heart",
+            destination: .simpleAnimation
+        ),
+        Topic(
+            title: "Spinning Animation",
+            description: "View a basic shape spinning on its x axis.",
+            systemSymbol: "arrow.left.arrow.right.circle",
+            destination: .spinningAnimation
+        ),
+        Topic(
+            title: "Explosion Animation",
+            description: "Tap to explode a rectangle into many pieces.",
+            systemSymbol: "sparkles",
+            destination: .explodingAnimation
+        ),
+        
+        Topic(
+            title: "Rollin' Rainbow",
+            description: "Explore an animation that makes a wave.",
+            systemSymbol: "wind",
+            destination: .rollingRainbow
+        ),
+        Topic(
+            title: "Drag N' Drop It",
+            description: "Use a drag gesture to move and animate a grid view.",
+            systemSymbol: "hand.draw",
+            destination: .dragnDropDots
+        ),
+        Topic(
+            title: "Dancing Dots",
+            description: "Explore an advanced animation that really pops.",
+            systemSymbol: "circle.grid.3x3",
+            destination: .dancingDots
+        )
+    ]
+}
+
 
 struct TopicOrganizer: View {
     var body: some View {
