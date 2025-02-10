ca67b43ae8453fb7e15caa09578368452f7ae089
Alex <oleksii.pidhornyi@gmail.com>
2025-02-10 19:49:12 +0100

    add spinning ellipse

--- AnimatingShapes/SpinningView.swift
+++ AnimatingShapes/SpinningView.swift
@@ -15,6 +15,21 @@ struct ResetEllipse: View {
     }
 }
 
+struct SpinningEllipse: View {
+    @State private var width: CGFloat = 200
+    
+    var body: some View {
+        Ellipse()
+            .frame(width: width, height: 200)
+            .foregroundColor(.orange)
+            .onAppear {
+                withAnimation(.easeInOut.repeatForever(autoreverses: true).speed(2)) {
+                    width = 0
+                }
+            }
+    }
+}
+
 struct SpinningView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
