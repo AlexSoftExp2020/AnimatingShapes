e17d67f3787e903a63d621e8d629907aac6de43a
Alex <oleksii.pidhornyi@gmail.com>
2025-02-11 12:24:45 +0100

    refactor composing shapes view

--- AnimatingShapes/ComposingShapesView.swift
+++ AnimatingShapes/ComposingShapesView.swift
@@ -11,20 +11,21 @@ struct ComposingShapesView: View {
     
     private let arcCount = 6
     private let colors: [Color] = [.red, .orange, .yellow, .green, .blue, .purple]
-    
-    var body: some View {
-        VStack {
-            ZStack {
-                ForEach(0..<arcCount, id: \.self) { arc in
-                    Circle()
-                }
-            }
-            VStack {
-                
-            }
-        }
-    }
-}
+     
+     var body: some View {
+         VStack {
+             ZStack {
+                 ForEach(0..<arcCount, id: \.self) { arc in
+                     Circle()
+                     
+                 }
+             }
+             VStack {
+                 
+             }
+         }
+     }
+ }
 
 #Preview {
     ComposingShapesView()
